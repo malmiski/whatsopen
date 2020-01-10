d907f384e136f433587936f8a24fbff21bc4f035
Mohanad Almiski <malmiski@live.com>
2020-01-10 17:01:43 -0500

    Fixed html mistake wit h nav

--- navbar.php
+++ navbar.php
@@ -1,8 +1,6 @@
 <section id="hero">
 <div class="wo-nav-container container">
-	<div class="wo-logo-container">
 	<img class="wo-logo" src="img/logo.png">
-	</div
 <nav class="wo-nav">
 	<ul class="wo-nav-ul">
 		<li class="wo-nav-li"><a href="#" class="wo-nav-link wo-nav-active">Home</a></li>
